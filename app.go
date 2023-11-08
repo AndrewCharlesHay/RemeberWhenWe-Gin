655f7d05be90cf28100f4bbde6207800e392ef79
AndrewCharlesHay <39sumer3939@gmail.com>
2023-11-08 02:55:35 +0000

    test: init

--- app.go
+++ app.go
@@ -1,17 +1,16 @@
 package main
 
-import (
-  "net/http"
+import "github.com/gin-gonic/gin"
 
-  "github.com/gin-gonic/gin"
-)
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := setupRouter()
+	r.Run(":8080")
 }
