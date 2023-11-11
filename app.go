708e6a954a240682dfc49744b76b1580494cd831
AndrewCharlesHay <39sumer3939@gmail.com>
2023-11-11 19:05:39 +0000

    sign into aws

--- app.go
+++ app.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
 
 func setupRouter() *gin.Engine {
+    // Using the SDK's default configuration, loading additional config
+    // and credentials values from the environment variables, shared
+    // credentials, and shared configuration files
+    cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
+    if err != nil {
+        log.Fatalf("unable to load SDK config, %v", err)
+    }
+	log.Print(cfg)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
--- app_test.go
+++ app_test.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"context"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	// "github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/stretchr/testify/assert"
 	// "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
 
 func TestPingRoute(t *testing.T) {
 
-    // Using the SDK's default configuration, loading additional config
-    // and credentials values from the environment variables, shared
-    // credentials, and shared configuration files
-    cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
-    if err != nil {
-        log.Fatalf("unable to load SDK config, %v", err)
-    }
-	log.Print(cfg)
 
 	router := setupRouter()
 
@@ -32,4 +23,16 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+
+func TestAWSSetup(t *testing.T) {
+	// Initialize a new AWS session using the default shared config
+	sess, err := session.NewSession(&aws.Config{})
+	if err != nil {
+		t.Fatalf("Error creating AWS session: %v", err)
+	}
+
+	t.Logf("AWS setup is successful")
+	t.Logf(*sess.Config.Endpoint)
 }
